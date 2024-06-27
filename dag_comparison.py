819f1d6d6e644599799b14f5109addf9ec8f2472
Michael Foster <m.foster@sheffield.ac.uk>
2024-06-27 15:58:56 +0100

    fixed DAG comparison

--- dag_comparison.py
+++ dag_comparison.py
@@ -1,8 +1,8 @@
 import pygraphviz as pgv
 import sys
 
-dag1 = sys.argv[1]
-dag2 = sys.argv[2]
+dag1 = pgv.AGraph(sys.argv[1])
+dag2 = pgv.AGraph(sys.argv[2])
 
 for edge in dag1.edges():
     if edge not in dag2.edges():
