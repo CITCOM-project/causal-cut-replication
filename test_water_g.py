73547034fc5afb01d72183f23779ee8cddcd6681
Michael <jmafoster1@gmail.com>
2024-07-11 11:20:38 +0100

    Linalg errors

--- test_water_g.py
+++ test_water_g.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import water_g
+import pandas as pd
 
 
 class TestWaterG(unittest.TestCase):
@@ -11,16 +12,47 @@ class TestWaterG(unittest.TestCase):
 
     def test_setup_xo_t_do_eq(self):
         xo_t_do = water_g.setup_xo_t_do(self.assigned, self.assigned)
-        xo_t_do_fast = water_g.setup_xo_t_do_fast(self.assigned, self.assigned)
-        np.testing.assert_array_equal(xo_t_do, xo_t_do_fast)
+        np.testing.assert_array_equal(xo_t_do, [0, 0, 0, 0, 0])
 
     def test_setup_xo_t_do_sensor_1(self):
         xo_t_do = water_g.setup_xo_t_do(self.assigned, self.followed_1)
-        xo_t_do_fast = water_g.setup_xo_t_do_fast(self.assigned, self.followed_1)
-        print(xo_t_do, xo_t_do_fast)
-        np.testing.assert_array_equal(xo_t_do, xo_t_do_fast)
+        np.testing.assert_array_equal(xo_t_do, [0, 0, 1, None, None])
 
     def test_setup_xo_t_do_sensor_2(self):
         xo_t_do = water_g.setup_xo_t_do(self.assigned, self.followed_2)
-        xo_t_do_fast = water_g.setup_xo_t_do_fast(self.assigned, self.followed_2)
-        np.testing.assert_array_equal(xo_t_do, xo_t_do_fast)
+        np.testing.assert_array_equal(xo_t_do, [0, 0, 0, 1, None])
+
+    def test_setup_fault_time_fast(self):
+        min, max = 1, 2
+        df = pd.DataFrame({"values": [1, 1, 1, 0], "time": [15, 16, 17, 18]})
+        df["within_safe_range"] = df["values"].between(min, max)
+        df["fault_time"] = water_g.setup_fault_time_fast(df)
+        np.testing.assert_array_equal(
+            df["fault_time"], [17.999, 17.999, 17.999, 17.999]
+        )
+
+    def test_setup_fault_time_fast_broadcast(self):
+        min, max = 1, 2
+        df = pd.DataFrame(
+            {"id": [1, 1, 2, 2], "values": [1, 1, 1, 0], "time": [15, 16, 17, 18]}
+        )
+        df["within_safe_range"] = df["values"].between(min, max)
+        df["fault_time"] = df.groupby("id").apply(water_g.setup_fault_time_fast).values
+
+        np.testing.assert_array_equal(
+            df["fault_time"], [30.999, 30.999, 17.999, 17.999]
+        )
+
+    def test_setup_fault_time_fast_no_fault(self):
+        min, max = 1, 2
+        df = pd.DataFrame({"values": [1, 1, 1], "time": [15, 16, 17]})
+        df["within_safe_range"] = df["values"].between(min, max)
+        df["fault_time"] = water_g.setup_fault_time_fast(df)
+        np.testing.assert_array_equal(df["fault_time"], [31.999, 31.999, 31.999])
+
+    def test_setup_fault_time_fast_fault_first(self):
+        min, max = 1, 2
+        df = pd.DataFrame({"values": [0, 1, 1], "time": [15, 16, 17]})
+        df["within_safe_range"] = df["values"].between(min, max)
+        df["fault_time"] = water_g.setup_fault_time_fast(df)
+        np.testing.assert_array_equal(df["fault_time"], [14.999, 14.999, 14.999])
--- water_g.py
+++ water_g.py
@@ -24,7 +24,11 @@ class Capability:
         self.time = time
 
     def __eq__(self, other):
-        return self.variable == other.variable and self.value == other.value and self.time == other.time
+        return (
+            self.variable == other.variable
+            and self.value == other.value
+            and self.time == other.time
+        )
 
     def __repr__(self):
         return f"({self.variable}, {self.value}, {self.time})"
@@ -49,7 +53,10 @@ class CapabilityList:
         self.capabilities[inx].value = value
 
     def treatment_strategy(self, inx, value):
-        strategy = CapabilityList(self.timesteps_per_intervention, [(c.variable, c.value) for c in self.capabilities])
+        strategy = CapabilityList(
+            self.timesteps_per_intervention,
+            [(c.variable, c.value) for c in self.capabilities],
+        )
         strategy.set_value(inx, value)
         return strategy
 
@@ -58,7 +65,9 @@ class CapabilityList:
 
 
 def setup_xo_t_do(strategy_assigned, strategy_followed):
-    mask = pd.Series(strategy_assigned) != pd.Series(strategy_followed)
+    mask = (pd.Series(strategy_assigned) != pd.Series(strategy_followed)).astype(
+        "boolean"
+    )
     false = mask.loc[mask == True]
     if false.empty:
         return np.zeros(len(mask) + 2)
@@ -84,20 +93,22 @@ def setup_fault_time(values, min, max):
     fault_time = None
     for inx, value in values.items():
         if not (min <= value <= max):
-            fault_time = inx
-            break
-    if fault_time is None:
-        fault_time = individual.time.max() + timesteps_per_intervention
-    fault_time -= 0.001
+            return inx
     return fault_time
 
 
-def setup_fault_time_fast(individual, perturbation=-0.001):
-    print(individual)
-    assert False
+def setup_fault_time_fast(
+    individual, timesteps_per_intervention=15, perturbation=-0.001
+):
     fault = individual[individual["within_safe_range"] == False]
-    fault_time = fault.index[0] if not fault.empty else (individual.time.max() + timesteps_per_intervention)
-    return fault_time + perturbation
+    fault_time = (
+        individual.time.loc[fault.index[0]]
+        if not fault.empty
+        else (individual.time.max() + timesteps_per_intervention)
+    )
+    return pd.DataFrame(
+        {"fault_time": np.repeat(fault_time + perturbation, len(individual))}
+    )
 
 
 def setup_recent_prog_t_dc(now_prog_t_dc):
@@ -109,31 +120,35 @@ def setup_recent_prog_t_dc(now_prog_t_dc):
     return result
 
 
-def preprocess_data(df, control_strategy, treatment_strategy, outcome, timesteps_per_intervention):
+def preprocess_data(
+    df, control_strategy, treatment_strategy, outcome, timesteps_per_intervention
+):
     df["trtrand"] = None  # treatment/control arm
     df["fault_t_do"] = None  # did a fault occur here?
-    df["xo_t_do"] = None  # did the individual deviate from the treatment of interest here?
+    df[
+        "xo_t_do"
+    ] = None  # did the individual deviate from the treatment of interest here?
     df["now_prog_t_dc"] = None  # has the situation progressed now?
     df["recent_prog_t_dc"] = None  # has the situation progressed in the past?
     # df["fault_time"] = None  # when did a fault occur?
 
-    df["within_safe_range"] = df[outcome].between(safe_ranges[outcome]["lo"], safe_ranges[outcome]["hi"])
-    df["fault_time"] = df.groupby("id")["within_safe_range"].transform(setup_fault_time_fast)
+    df["within_safe_range"] = df[outcome].between(
+        safe_ranges[outcome]["lo"], safe_ranges[outcome]["hi"]
+    )
+    df["fault_time"] = (
+        df.groupby("id")[["within_safe_range", "time"]]
+        .apply(setup_fault_time_fast)
+        .values
+    )
+    assert not pd.isnull(df["fault_time"]).any()
 
     individuals = []
     new_id = 0
     print("  Preprocessing groups")
-    for id, individual in tqdm(df.groupby("id")):
-        # fault_time = setup_fault_time(individual[outcome], safe_ranges[outcome]["lo"], safe_ranges[outcome]["hi"])
-        assert len(set(individual["fault_time"])) == 1
-        fault_time = individual["fault_time"].iloc[0]
-        if fault_time <= 0:  # Skip individuals who are faulty at time 0 (analogous to a patient who is already dead)
-            continue
-        # individual["fault_time"] = fault_time
-        # individual["fault_time_fast"] = fault_time_fast
-
+    for id, individual in tqdm(df.query("fault_time > 0").groupby("id")):
         individual = individual.loc[
-            (individual.time % timesteps_per_intervention == 0) & (individual.time <= control_strategy.total_time())
+            (individual.time % timesteps_per_intervention == 0)
+            & (individual.time <= control_strategy.total_time())
         ].copy()
 
         individual["fault_t_do"] = setup_fault_t_do(
@@ -141,61 +156,82 @@ def preprocess_data(df, control_strategy, treatment_strategy, outcome, timesteps
         )
         assert (
             sum(individual["fault_t_do"]) <= 1
-        ), f"Error initialising fault_t_do for id {id} with fault at {fault_time}"
+        ), f"Error initialising fault_t_do for id {id} with fault at {individual.fault_time.iloc[0]}"
         individual["now_prog_t_dc"] = setup_fault_t_do(
             individual[outcome], safe_ranges[outcome]["lo"], safe_ranges[outcome]["hi"]
         )
-        individual["recent_prog_t_dc"] = setup_recent_prog_t_dc(individual["now_prog_t_dc"])
+        individual["recent_prog_t_dc"] = setup_recent_prog_t_dc(
+            individual["now_prog_t_dc"]
+        )
         faulty = individual.loc[
-            ~individual[outcome].between(safe_ranges[outcome]["lo"], safe_ranges[outcome]["hi"]),
+            ~individual[outcome].between(
+                safe_ranges[outcome]["lo"], safe_ranges[outcome]["hi"]
+            ),
             "time",
         ]
-
-        if fault_time <= 0:
-            continue
 
         # Control flow:
         # Individuals that start off in both arms, need cloning (hence incrementing the ID within the if statements)
         # Individuals that don't start off in either arm need leaving out (hence two ifs rather than elif or else)
         strategy = [
-            Capability(c.variable, individual.loc[individual.time == c.time, c.variable].values[0], c.time)
+            Capability(
+                c.variable,
+                individual.loc[individual.time == c.time, c.variable].values[0],
+                c.time,
+            )
             for c in treatment_strategy.capabilities
         ]
         if strategy[0] == control_strategy.capabilities[0]:
             individual["id"] = id
             id += 1
             individual["trtrand"] = 0
-            individual["xo_t_do"] = setup_xo_t_do(strategy, control_strategy.capabilities)
-            individuals.append(individual.loc[individual.time <= fault_time].copy())
+            individual["xo_t_do"] = setup_xo_t_do(
+                strategy, control_strategy.capabilities
+            )
+            individuals.append(
+                individual.loc[individual.time <= individual.fault_time].copy()
+            )
         if strategy[0] == treatment_strategy.capabilities[0]:
             individual["id"] = id
             id += 1
             individual["trtrand"] = 1
-            individual["xo_t_do"] = setup_xo_t_do(strategy, treatment_strategy.capabilities)
-            individuals.append(individual.loc[individual.time <= fault_time].copy())
+            individual["xo_t_do"] = setup_xo_t_do(
+                strategy, treatment_strategy.capabilities
+            )
+            individuals.append(
+                individual.loc[individual.time <= individual.fault_time].copy()
+            )
+    print(df[[outcome, "within_safe_range", "fault_time"]])
     df = pd.concat(individuals)
     df.to_csv("data/long_preprocessed.csv", index=False)
 
 
 def estimate_hazard_ratio(
-    novCEA, timesteps_per_intervention, fitBLswitch_formula, fitBLTDswitch_formula, print_summary=False
+    novCEA,
+    timesteps_per_intervention,
+    fitBLswitch_formula,
+    fitBLTDswitch_formula,
+    print_summary=False,
 ):
     # Use logistic regression to predict switching given baseline covariates
     print(f"  predict switching given baseline covariates: {fitBLswitch_formula}")
-    fitBLswitch = smf.logit(fitBLswitch_formula, data=novCEA[novCEA.trtrand == 0]).fit()
+    fitBLswitch = smf.logit(fitBLswitch_formula, data=novCEA).fit()
 
     # Estimate the probability of switching for each patient-observation included in the regression.
     novCEA["pxo1"] = fitBLswitch.predict(novCEA)
 
     # Use logistic regression to predict switching given baseline and time-updated covariates (model S12)
-    print(f"  predict switching given baseline and time-updated covariates: {fitBLTDswitch_formula}")
+    print(
+        f"  predict switching given baseline and time-updated covariates: {fitBLTDswitch_formula}"
+    )
     # Covariance matrix to examine colinearities
     # relevant_features = fitBLTDswitch_formula.split(" ~ ")[1].split(" + ")
     # novCEA[relevant_features].corr().round(3).to_csv("/tmp/corr.csv")
 
     try:
         fitBLTDswitch = smf.logit(
-            fitBLTDswitch_formula, data=novCEA  # [(novCEA.trtrand == 0) & (novCEA["recent_prog_t_dc"] == 1)],
+            fitBLTDswitch_formula,
+            data=novCEA,  # [(novCEA.trtrand == 0) & (novCEA["recent_prog_t_dc"] == 1)],
         ).fit()
     except np.linalg.LinAlgError:
         return None, None
@@ -215,8 +251,12 @@ def estimate_hazard_ratio(
     novCEA["num"] = prod["num"]
     novCEA["denom"] = prod["denom"]
 
-    assert not novCEA["num"].isnull().any(), f"{len(novCEA['num'].isnull())} null numerator values"
-    assert not novCEA["denom"].isnull().any(), f"{len(novCEA['denom'].isnull())} null denom values"
+    assert (
+        not novCEA["num"].isnull().any()
+    ), f"{len(novCEA['num'].isnull())} null numerator values"
+    assert (
+        not novCEA["denom"].isnull().any()
+    ), f"{len(novCEA['denom'].isnull())} null denom values"
 
     isControl = novCEA.trtrand == 0
     novCEA.loc[isControl, "weight"] = 1 / novCEA.denom[isControl]
@@ -228,15 +268,17 @@ def estimate_hazard_ratio(
 
     novCEA_KM = novCEA.loc[novCEA.xo_t_do == 0].copy()
     novCEA_KM["tin"] = novCEA_KM.time
-    novCEA_KM["tout"] = pd.concat([(novCEA_KM.time + timesteps_per_intervention), novCEA_KM.fault_time], axis=1).min(
-        axis=1
-    )
+    novCEA_KM["tout"] = pd.concat(
+        [(novCEA_KM.time + timesteps_per_intervention), novCEA_KM.fault_time], axis=1
+    ).min(axis=1)
 
     novCEA_KM.to_csv("/tmp/novCEA_KM.csv")
 
     assert (
         novCEA_KM["tin"] <= novCEA_KM["tout"]
-    ).all(), f"Left before joining\n{novCEA_KM.loc[novCEA_KM['tin'] >= novCEA_KM['tout']]}"
+    ).all(), (
+        f"Left before joining\n{novCEA_KM.loc[novCEA_KM['tin'] >= novCEA_KM['tout']]}"
+    )
 
     novCEA_KM.dropna(axis=1, inplace=True)
     novCEA_KM.replace([float("inf")], 100, inplace=True)
@@ -288,16 +330,27 @@ if __name__ == "__main__":
             print(f"Missing data for {outcome}")
             continue
 
-        if len(df.loc[(df[outcome] < safe_ranges[outcome]["lo"]) | (df[outcome] > safe_ranges[outcome]["hi"])]) == 0:
+        if (
+            len(
+                df.loc[
+                    (df[outcome] < safe_ranges[outcome]["lo"])
+                    | (df[outcome] > safe_ranges[outcome]["hi"])
+                ]
+            )
+            == 0
+        ):
             print(
                 f"  No faults with {outcome}. Cannot perform estimation.\n"
                 f"  Observed range [{df[outcome].min()}, {df[outcome].max()}].\n"
                 f"  Safe range {safe_ranges[outcome]}"
             )
             continue
-        if len(df.loc[(df[outcome] < safe_ranges[outcome]["lo"]) | (df[outcome] > safe_ranges[outcome]["hi"])]) == len(
-            df
-        ):
+        if len(
+            df.loc[
+                (df[outcome] < safe_ranges[outcome]["lo"])
+                | (df[outcome] > safe_ranges[outcome]["hi"])
+            ]
+        ) == len(df):
             print(
                 f"  All faults with {outcome}. Cannot perform estimation.\n"
                 f"  Observed range [{df[outcome].min()}, {df[outcome].max()}].\n"
@@ -322,17 +375,32 @@ if __name__ == "__main__":
 
                 # Treatment strategy is the same, but with one capability negated
                 # i.e. we examine the counterfactual "What if we had not done that?"
-                treatment_strategy = control_strategy.treatment_strategy(i, int(not capability.value))
+                treatment_strategy = control_strategy.treatment_strategy(
+                    i, int(not capability.value)
+                )
                 print("  TREATMENT STRATEGY", treatment_strategy.capabilities)
                 datum["treatment_strategy"] = treatment_strategy.capabilities
-                preprocess_data(df, control_strategy, treatment_strategy, outcome, timesteps_per_intervention)
+                print(
+                    "SAFE RANGE", safe_ranges[outcome]["lo"], safe_ranges[outcome]["hi"]
+                )
+                preprocess_data(
+                    df,
+                    control_strategy,
+                    treatment_strategy,
+                    outcome,
+                    timesteps_per_intervention,
+                )
 
                 novCEA = pd.read_csv("data/long_preprocessed.csv")
 
-                print(f"  {novCEA['fault_t_do'].sum()}/{len(novCEA.groupby('id'))} faulty runs observed")
+                print(
+                    f"  {novCEA['fault_t_do'].sum()}/{len(novCEA.groupby('id'))} faulty runs observed"
+                )
 
                 for id, group in novCEA.groupby("id"):
-                    assert sum(group["fault_t_do"]) <= 1, f"Multiple fault times for id {id}\n{group}"
+                    assert (
+                        sum(group["fault_t_do"]) <= 1
+                    ), f"Multiple fault times for id {id}\n{group}"
 
                 fitBLswitch_formula = "xo_t_do ~ time"
 
@@ -344,13 +412,20 @@ if __name__ == "__main__":
                     neighbours.remove("AIT402")
                     neighbours.remove("FIT401")
 
-                assert len(neighbours) > 0, f"No neighbours for node {capability.variable}"
+                assert (
+                    len(neighbours) > 0
+                ), f"No neighbours for node {capability.variable}"
 
-                fitBLTDswitch_formula = f"{fitBLswitch_formula} + {' + '.join(neighbours)}"
+                fitBLTDswitch_formula = (
+                    f"{fitBLswitch_formula} + {' + '.join(neighbours)}"
+                )
                 datum["fitBLTDswitch_formula"] = fitBLTDswitch_formula
 
                 params, confidence_intervals = estimate_hazard_ratio(
-                    novCEA, timesteps_per_intervention, fitBLswitch_formula, fitBLTDswitch_formula
+                    novCEA,
+                    timesteps_per_intervention,
+                    fitBLswitch_formula,
+                    fitBLTDswitch_formula,
                 )
                 if params is None:
                     print("  FAILURE: Params was None")
@@ -379,12 +454,17 @@ if __name__ == "__main__":
                     pool = Pool()
                     params_repeats = pool.map(
                         estimate_hazard_ratio_parallel,
-                        [np.random.choice(ids, len(ids), replace=True) for x in range(num_repeats)],
+                        [
+                            np.random.choice(ids, len(ids), replace=True)
+                            for x in range(num_repeats)
+                        ],
                     )
                     params_repeats = [x for x in params_repeats if x is not None]
                     datum["params_repeats"] = [p.to_dict() for p in params_repeats]
 
-                    datum["mean_risk_ratio"] = (sum(params_repeats) / len(params_repeats)).to_dict()
+                    datum["mean_risk_ratio"] = (
+                        sum(params_repeats) / len(params_repeats)
+                    ).to_dict()
                     datum["kurtosis"] = kurtosis(params_repeats).tolist()
                     datum["successes"] = len(params_repeats)
                 data.append(datum)
