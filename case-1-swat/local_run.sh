fd5cf08b26a6248125e05027bda7c8e82fa1a567
Michael Foster <m.foster@sheffield.ac.uk>
2024-08-16 14:18:44 +0100

    fixed local run

--- case-1-swat/local_run.sh
+++ case-1-swat/local_run.sh
@@ -1,6 +1,6 @@
 mkdir -p logs/attack_data
 mkdir -p logs/attack_normal_data
-mkdir -p logs/attack_mutated_normal_data
+mkdir -p logs/attack_mutated_attack_data
 mkdir -p logs/attack_mutated_normal_data
 for i in {0..30}
 do
@@ -9,7 +9,7 @@ do
   # This should run in a couple of days
   python water_g.py -a successful_attacks_flat.json    -A -i $i -o logs/attack_normal_data/attack_$i.json         ../data/attack_normal_data_105.csv
   # This should run in a few hours
-  python water_g.py -a successful_attacks_mutated.json -A -i $i -o logs/attack_mutated_normal_data/attack_$i.json ../data/attack_data_105.csv
+  python water_g.py -a successful_attacks_mutated.json -A -i $i -o logs/attack_mutated_attack_data/attack_$i.json ../data/attack_data_105.csv
   # This should run in a couple of days
   python water_g.py -a successful_attacks_mutated.json -A -i $i -o logs/attack_mutated_normal_data/attack_$i.json ../data/attack_normal_data_105.csv
 done
