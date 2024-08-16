76194d1e0215347c3685cd19228d2d67f4ffc59f
Michael Foster <m.foster@sheffield.ac.uk>
2024-08-16 08:53:01 +0100

    fixed mkdir

--- case-1-swat/hpc-submissions.sh
+++ case-1-swat/hpc-submissions.sh
@@ -1,6 +1,6 @@
 mkdir -p logs/attack_data
 mkdir -p logs/attack_normal_data
-mkdir -p logs/attack_mutated_normal_data
+mkdir -p logs/attack_mutated_attack_data
 mkdir -p logs/attack_mutated_normal_data
 for i in {0..30}
 do
