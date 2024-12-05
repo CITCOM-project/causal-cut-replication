2e4b221cda67d6082f8b94228f02b34ff4ede25d
Michael Foster <m.foster@sheffield.ac.uk>
2024-12-05 17:00:56 +0000

    Added number of sim runs to log postprocessor

--- case-2-oref0/scripts/log_postprocessor.py
+++ case-2-oref0/scripts/log_postprocessor.py
@@ -4,37 +4,12 @@ This module checks the estimated sequences of attacks to see which ones still yi
 
 import json
 import sys
-import pandas as pd
-from uuid import uuid4
 from itertools import product
+
+import pandas as pd
 
 from fault_finder import reproduce_fault
 from aps_digitaltwin.util import intervention_values
-
-
-def check_if_still_fault(attack: dict):
-    """
-    Run the attack and check if the interventions estimated significant lead to a fault.
-
-    :param attack: dict with details of the attack and causal effect estimate.
-    """
-    interventions = []
-    for treatment_strategy in attack["treatment_strategies"]:
-        if "result" not in treatment_strategy or not (
-            treatment_strategy["result"]["ci_low"][0] < 1 < treatment_strategy["result"]["ci_high"][0]
-        ):
-            interventions.append(attack["attack"][treatment_strategy["intervention_index"]])
-    interventions = [(t, v, intervention_values[v]) for t, v, _ in interventions]
-    still_fault, _ = reproduce_fault(
-        timesteps=500,
-        initial_carbs=attack["initial_carbs"],
-        initial_bg=attack["initial_bg"],
-        initial_iob=attack["initial_iob"],
-        interventions=interventions,
-        constants=attack["constants"],
-        save_path=f"/tmp/{uuid4()}.csv",
-    )
-    return still_fault, len(interventions)
 
 
 def build_attack(attack: dict):
@@ -82,6 +57,8 @@ def build_attack(attack: dict):
     attack["pure_estimate_fault"] = still_fault
     attack["estimated_interventions"] = list(interventions)
 
+    simulator_runs = 1
+
     interventions_to_add = list(treatment_strategies["intervention_index"])
     while not still_fault and interventions_to_add:
         t, v, _ = attack["attack"][interventions_to_add.pop(0)]
@@ -94,13 +71,13 @@ def build_attack(attack: dict):
             constants=attack["constants"],
             interventions=interventions,
         )
+        simulator_runs += 1
     attack["extended_estimate_fault"] = still_fault
     attack["extended_interventions"] = list(interventions)
+    attack["simulator_runs"] = simulator_runs
 
     # Further minimise the trace by considering all combinations of the remaining interventions, starting with the
-    # minimum number of interventions and gradually working back up. This step is necessary because the greedy
-    # method above can sometimes mark spurious events as necessary causes, presumably due to interactions between
-    # interventions. However, it is too expensive to start off simply by considering combinations.
+    # minimum number of interventions and gradually working back up.
     minimal = dict(enumerate(interventions))
     minimal_keys = sorted(list(minimal.keys()))
     for mask in sorted(list(product([0, 1], repeat=len(minimal))), key=sum)[1:]:
